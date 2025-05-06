759877e255e7420842db34014f88998c60748996
Tyler R <tyler73750@gmail.com>
2025-05-06 14:23:35 -0500

    crossword

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useRef } from 'react';
+import Crossword from '@jaredreisinger/react-crossword';
+import { data } from './data.js';
+import './App.css';
+import { Modal, Box, Typography, Button } from '@mui/material';
+import 'normalize.css';
+
+
+function App() {
+  const [completedCrossword, setCompletedCrossword] = useState(false);
+
+  const crosswordRef = useRef(null);
+
+  function resetCrossword() {
+    console.log(crosswordRef)
+    crosswordRef.current.reset();
+    setCompletedCrossword(false);
+  }
+
+  function checkCrossword() {
+    if (crosswordRef.current.isCrosswordCorrect()) {
+      setCompletedCrossword(true);
+    }
+  }
+
+
+  return (
+      <Box
+        sx={{
+          minHeight: '100vh',
+          minWidth: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 2,
+          backgroundColor: '#FFF3E0'
+        }}
+      >
+        <Typography variant="h5" align="center" 
+          sx={{ fontFamily: '"Bagel Fat One", sans-serif', color: '#FF4081'}}
+        >
+         ❤️ Crossword For the best girlfriend in the world ❤️
+        </Typography>
+        <Box>
+          <Crossword 
+            style={{width: '100vh'}}
+            theme={{
+              cellBackground: '#FFF3E0',
+              numberColor: '#FF4081',
+              highlightBackground: ' #76FF03', //#FFEB3B',
+            }}
+            ref={crosswordRef} 
+            data={data} 
+            onCrosswordCorrect={checkCrossword}
+          />
+        </Box>
+        <Modal open={completedCrossword}>
+          <Box
+            sx={{
+              position: 'absolute',
+              justifyContent: 'center',
+              alignItems: 'center',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              bgcolor: 'background.paper',
+              border: '2px solid #000',
+              boxShadow: 24,
+              p: 4,
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              backgroundColor: '#FFF3E0'
+            }}
+          >
+            <Typography variant="h5" 
+              sx={{ fontFamily: '"Bagel Fat One", sans-serif', color: '#FF4081'}}
+            >
+              Love you Boops!!!
+            </Typography>
+            <Box component="img" src="raccoon.jpg" sx={{ width: 200}}/>
+            <Button variant="contained" onClick={resetCrossword}>
+              <Typography variant="h5" 
+                sx={{ fontFamily: '"Bagel Fat One", sans-serif' }}
+              >
+                GET ME OUTTA HERE!
+              </Typography>
+            </Button>
+          </Box>
+        </Modal>
+      </Box>
+  );
+}
+
+export default App;
+
---
+++ src/data.js
@@ -0,0 +1,224 @@
+export const data = {
+  across: {
+    1: {
+      clue: 'What I swore I would not call you but I do all the time now!',
+      answer: 'BOOPS',
+      row: 0,
+      col: 10,
+    },
+    2: {
+      clue: 'What is your favorite flower?',
+      answer: 'TULIP',
+      row: 3,
+      col: 10,
+    },
+    4: {
+      clue: 'How long will I love you ;)',
+      answer: 'FOREVER',
+      row: 4,
+      col: 2,
+    },
+    7: {
+      clue: 'Where was our first date?',
+      answer: 'VOLCANO',
+      row: 6,
+      col: 9,
+    },
+    8: {
+      clue: 'Shipname?!?!',
+      answer: 'BRITTLER',
+      row: 7,
+      col: 0,
+    },
+    9: {
+      clue: 'What describes your aesthetic?',
+      answer: 'BADDIE',
+      row: 8,
+      col: 9,
+    },
+    10: {
+      clue: 'When I answer the phone: "Hey, ___"',
+      answer: 'SHMUCKUMS',
+      row: 10,
+      col: 6,
+    },
+  },
+  down: {
+    1: {
+      clue: 'Who is my favorite person?',
+      answer: 'BRITT',
+      row: 0,
+      col: 10,
+    },
+    3: {
+      clue: 'Another phrase used when answering the phone: "Ohhh, ___!"',
+      answer: 'PWINCESS',
+      row: 3,
+      col: 14,
+    },
+    5: {
+      clue: 'What\'s my favorite thing about you?',
+      answer: 'EVERYTHING',
+      row: 4,
+      col: 7,
+    },
+    6: {
+      clue: 'Where did we first spot one another',
+      answer: 'HINGE',
+      row: 6,
+      col: 2,
+    },
+    11: {
+      clue: 'What do your eyes remind me of / What\'s your favorite drink?',
+      answer: 'MATCHA',
+      row: 10,
+      col: 13,
+    },
+  }
+};
+
+export const badData = {
+  across: {
+    1: {
+      clue: 'What do your eyes remind me of/ What\'s your favorite drink?',
+      answer: 'MATCHA',
+      row: 0,
+      col: 0,
+    },
+    3: {
+      clue: 'What is your favorite flower?',
+      answer: 'TULIP',
+      row: 1,
+      col: 0,
+    },
+    5: {
+      clue: 'Where did we first spot one another?',
+      answer: 'HINGE',
+      row: 2,
+      col: 0,
+    },
+    7: {
+      clue: 'Where was our first date?',
+      answer: 'VOLCANO',
+      row: 3,
+      col: 0,
+    },
+    9: {
+      clue: 'How long will I love you?',
+      answer: 'FOREVER',
+      row: 4,
+      col: 0,
+    },
+    11: {
+      clue: 'My favorite feature about you?',
+      answer: 'EVERYTHING',
+      row: 5,
+      col: 0,
+    },
+    13: {
+      clue: 'Who is my favorite person?',
+      answer: 'BRITT',
+      row: 6,
+      col: 0,
+    },
+    15: {
+      clue: 'What describes your aesthetic?',
+      answer: 'BADDIE',
+      row: 7,
+      col: 0,
+    },
+    17: {
+      clue: 'Shipname?!?',
+      answer: 'BRITTLER',
+      row: 8,
+      col: 0,
+    },
+    19: {
+      clue: 'When I answer the phone: "Hey, ___"?',
+      answer: 'SHMUCKUMS',
+      row: 9,
+      col: 0,
+    },
+    21: {
+      clue: 'Another phrase used when answering the phone:',
+      answer: 'PWINCESS',
+      row: 10,
+      col: 0,
+    },
+    23: {
+      clue: 'What I swore I would not call you but I do all the time now:',
+      answer: 'BOOPS',
+      row: 11,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: 'What describes your aesthetic?',
+      answer: 'BADDIE',
+      row: 0,
+      col: 7,
+    },
+    4: {
+      clue: 'Where was our first date?',
+      answer: 'VOLCANO',
+      row: 0,
+      col: 3,
+    },
+    6: {
+      clue: 'Who is my favorite person?',
+      answer: 'BRITT',
+      row: 0,
+      col: 6,
+    },
+    8: {
+      clue: 'What do your eyes remind me of/ What\'s your favorite drink?',
+      answer: 'MATCHA',
+      row: 0,
+      col: 0,
+    },
+    10: {
+      clue: 'Shipname?!?',
+      answer: 'BRITTLER',
+      row: 0,
+      col: 8,
+    },
+    12: {
+      clue: 'How long will I love you?',
+      answer: 'FOREVER',
+      row: 0,
+      col: 4,
+    },
+    16: {
+      clue: 'When I answer the phone: "Hey, ___"?',
+      answer: 'SHMUCKUMS',
+      row: 0,
+      col: 9,
+    },
+    18: {
+      clue: 'Another phrase used when answering the phone:',
+      answer: 'PWINCESS',
+      row: 0,
+      col: 10,
+    },
+    20: {
+      clue: 'What is your favorite flower?',
+      answer: 'TULIP',
+      row: 1,
+      col: 0,
+    },
+    22: {
+      clue: 'Where did we first spot one another?',
+      answer: 'HINGE',
+      row: 2,
+      col: 0,
+    },
+    24: {
+      clue: 'My favorite feature about you?',
+      answer: 'EVERYTHING',
+      row: 5,
+      col: 0,
+    },
+  },
+};
+
